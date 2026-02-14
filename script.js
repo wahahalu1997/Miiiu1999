61d3f41034c37b3349243d16d82acd02ae7810e4
wahahalu1997 <linleo1989@gmail.com>
2026-02-15 04:43:19 +0800

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ const gifStages = [
 const noMessages = [
     "No",
     "Are you positive? 🤔",
-    "Pookie please... 🥺",
+    "Bonita  please... 🥺",
     "If you say no, I will be really sad...",
     "I will be very sad... 😢",
     "Please??? 💔",
